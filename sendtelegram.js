79ca051d2004a01f90385fc60d075629e776e499
mustafoa <mustafo2008@gmail.com>
2022-04-17 15:28:44 -0400

    edit files

---
+++ sendtelegram.js
@@ -0,0 +1,134 @@
+window.addEventListener("load", () =>{
+    document.querySelector(".main").classList.remove("hidden");
+    document.querySelector(".home-section").classList.add("active");
+    // Page Loader
+    document.querySelector(".page-loader").classList.add("fade-out");
+    setTimeout(() =>{
+        document.querySelector(".page-loader").style.display = "none";
+    },600);
+});
+
+// Toggle Navbar
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", () => {
+    hideSection();
+    toggleNavbar();
+    document.body.classList.toggle("hide-scrolling");
+});
+
+function hideSection() {
+    document.querySelector("section.active").classList.toggle("fade-out");
+}
+function toggleNavbar() {
+    document.querySelector(".header").classList.toggle("active");
+}
+
+                        // Active Section
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("link-item") && e.target.hash !== ""){
+        // Overlay
+        document.querySelector(".overlay").classList.add("active");
+        navToggler.classList.add("hide");
+        if (e.target.classList.contains("nav-item")){
+            toggleNavbar();
+        }
+        else {
+            hideSection();
+            document.body.classList.add("hide-scrolling");
+        }
+        setTimeout(() => {
+            document.querySelector("section.active").classList.remove("active", "fade-out");
+            document.querySelector(e.target.hash).classList.add("active");
+            window.scrollTo(0,0);
+            document.body.classList.remove("hide-scrolling");
+            navToggler.classList.remove("hide");
+            document.querySelector(".overlay").classList.remove("active");
+        },500);
+    }
+});
+
+
+// About Tabs
+const tabsContainer = document.querySelector(".about-tabs"),
+    aboutSection = document.querySelector(".about-section");
+
+tabsContainer.addEventListener("click", (e) => {
+    if (e.target.classList.contains("tab-item") && !e.target.classList.contains("active")) {
+        tabsContainer.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
+        const target = e.target.getAttribute("data-target");
+        aboutSection.querySelector(".tab-content.active").classList.remove("active");
+        aboutSection.querySelector(target).classList.add("active");
+    }
+});
+
+// Portfolio Items Popup
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("view-project-btn")) {
+        togglePortfolioPopup();
+        document.querySelector(".portfolio-popup").scrollTo(0, 0);
+        portfolioItemDetails(e.target.parentElement);
+    }
+})
+function togglePortfolioPopup() {
+    document.querySelector(".portfolio-popup").classList.toggle("open");
+    document.body.classList.toggle("hide-scrolling");
+    document.querySelector(".main").classList.toggle("fade-out");
+}
+document.querySelector(".pp-close").addEventListener("click", togglePortfolioPopup);
+
+// hide popup when clicking outside of it
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("pp-inner")) {
+        togglePortfolioPopup();
+    }
+});
+
+function portfolioItemDetails(portfolioItem) {
+    document.querySelector(".pp-thumbnail img").src =
+        portfolioItem.querySelector(".portfolio-item-thumbnail img").src;
+
+    document.querySelector(".pp-header h3").innerHTML =
+        portfolioItem.querySelector(".portfolio-item-title").innerHTML;
+
+    document.querySelector(".pp-body").innerHTML =
+        portfolioItem.querySelector(".portfolio-item-details").innerHTML;
+}
+
+
+// Telegram Bot
+var telegram_bot_id = "5140293099:AAGqaTJBYGLwDprRPyrbTL_piLg2ZGzZvxM";
+var chat_id = 976376314;
+var u_name, email, subject, message;
+var ready = function() {
+    u_name = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    subject = document.getElementById("subject").value;
+    message = document.getElementById("message").value;
+    message = "Name: " + u_name + "\nEmail: " + email + "\nSubject: " + subject + "\nMessage: " + message;
+};
+var sendtelegram = function() {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function(response) {
+        console.log(response);
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("subject").value = "";
+    document.getElementById("message").value = "";
+    return false;
+};
